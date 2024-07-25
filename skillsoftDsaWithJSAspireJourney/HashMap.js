a91c611fe21450cdaa8002bb91b6d75341746357
Oheneba Poku-Marboah <ohenebakob@hotmail.com>
2024-07-25 22:05:13 +0300

    Hashmaps collision resolution - looping through linked list in separate chaining

---
+++ skillsoftDsaWithJSAspireJourney/HashMap.js
@@ -0,0 +1,63 @@
+const LinkedList = require('./LinkedList');
+const Node = require('./Node');
+
+class HashMap {
+    constructor(size = 0) {
+        this.hashmap = new Array(size)
+        .fill(null)
+        .map(() => new LinkedList());
+    }
+  
+    hash(key) {
+        let hashCode = 0;
+        for (let i = 0; i < key.length; i++) {
+            // The hashCode += allows the hashing function to avoid generating duplicate hashCodes
+            // if keys have the same characters in different orders, such as bat and tab.
+            hashCode += hashCode + key.charCodeAt(i);      
+        }
+        return hashCode % this.hashmap.length;
+    }
+
+    assign(key, value) {
+        const arrayIndex = this.hash(key);
+        const linkedList = this.hashmap[arrayIndex];
+        if (linkedList.head === null) {
+          linkedList.addToHead({ key, value });
+          return;
+        }
+        let current= linkedList.head;
+
+        while(current !== null){
+            if (current.data.key === key){
+                current.data.value = value;
+            }
+            if(current.getNextNode() === null){
+                //it is tail
+                const lastNode = new Node({ key, value });
+                current.setNextNode(lastNode);
+                break;
+            }
+            current= current.getNextNode()
+        }
+        
+    }
+
+    retrieve (key){
+        const arrayIndex =  this.hash(key);
+        return this.hashmap[arrayIndex];
+    }
+  
+}
+
+// // test the retrieve function 
+// const glossary = new  HashMap(3);
+// glossary.assign('semordnilap', 'Words that form different words when reversed');
+// console.log(glossary.retrieve('semordnilap'))
+
+// // test the assign function 
+// const employees = new HashMap(3);
+// employees.assign('34-567', 'Mara');
+// console.log(employees.hashmap)
+  
+module.exports = HashMap;
+  
